d9f8456fc9d26fdc929eb8d6428a7c1ae110efb9
Sanjay Sudhakaran <sanjaysudhakaran@gmail.com>
2019-11-01 15:02:37 +1300

    Added pizza option

--- php/order_page.php
+++ php/order_page.php
@@ -21,7 +21,48 @@
 <?php include "../php_components/navbar.php";
     include "../php_components/check_ingredients.php" ?>
     <div class="container">
-        <p>Order page</p>
+       <h1>Place Order</h1>
+       <form action="test.php" method="POST">
+           <div class="row">
+                <div class="card col-sm" style="width: 18rem;">
+                    <img src="../assets/img/pizzaSample.jpeg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <label for="pizzaRadioBtn">Vegan Mozzarella</label>
+                        <input type="radio" name="pizza" id="pizzaRadioBtn" value="Vegan Mozzarella">
+                        
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+                <div class="card col-sm" style="width: 18rem;">
+                    <img src="../assets/img/pizzaSample2.jpeg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <label for="pizzaRadioBtn">Classic Vegan</label>
+                        <input type="radio" name="pizza" id="pizzaRadioBtn" value="Classic Vegan">
+                        
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+                <div class="card col-sm" style="width: 18rem;">
+                    <img src="../assets/img/pizzaSample3.jpeg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <label for="pizzaRadioBtn">Vegetarian Delight</label>
+                        <input type="radio" name="pizza" id="pizzaRadioBtn" value="Vegetarian Delight">
+                        
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+                <div class="card col-sm" style="width: 18rem;">
+                    <img src="../assets/img/pizzaSample3.jpeg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <label for="pizzaRadioBtn">Make Your Own!</label>
+                        <input type="radio" name="pizza" id="pizzaRadioBtn" value="Custom Pizza">
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+            
+            <input type="submit" name="submit" value="Place Order">
+        </form>
 
     </div>
 </body>
