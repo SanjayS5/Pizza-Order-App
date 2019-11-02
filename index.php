b0eca8bcf04bf357c40c902d6c1912c2ade868ba
Maleerat Lertruedeeporn <maleerat.l.123@gmail.com>
2019-11-02 16:43:52 +1300

    indexm

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link rel="stylesheet" type="text/css" href="css/index.css">
   <title>Document</title>
 </head>
 <header>
@@ -14,20 +15,38 @@
 
 <body>
 
+  <div class="row main-top justify-content-center align-items-center">
+    <div class="page-header">
+      <h1>Pizzeria Fortuna</h1>
+      <form action="php/order_page.php" method="POST">
+        <button type="submit" class="btn place-order bg-dark">Place Order</button>
+      </form>
+    </div>
+    <div>
+    </div>
+  </div>
+
 <div class="container">
-  <div class="page-header">
-    <h1>Pizzeria Fortuna</h1>
-  </div>
-  <div>
-    <form action="php/order_page.php" method="POST">
-      <button type="submit" class="btn btn-secondary">Place Order</button>
-    </form>
-  </div>
-  <div>
+  <div class="our-story">
     <h2>Our Story</h2>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus voluptatem pariatur fuga corrupti alias deserunt, labore porro placeat provident optio. Ut fugiat magni sint vel quos ullam, laborum harum quas.
-    </p>
+
+        <div class="image-story">
+            <img src="assets/img/ourStory.jpg" alt="" >
+        </div>
+    <div class="describe-story">
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus voluptatem pariatur
+        fuga corrupti alias deserunt, labore porro placeat provident optio. Ut fugiat magni sint
+        vel quos ullam, laborum harum quas.Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Temporibus voluptatem pariatur fuga corrupti alias deserunt, labore porro placeat provident
+        optio. Ut fugiat magni sint vel quos ullam, laborum harum quas.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus voluptatem pariatur
+        fuga corrupti alias deserunt, labore porro placeat provident optio. Ut fugiat magni sint
+        vel quos ullam, laborum harum quas.Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      </p>
+    </div>
   </div>
 </div>
 
@@ -36,9 +55,9 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
-<footer class="footer mt-auto py-3">
+<footer class="footer mt-auto py-3 navbar-dark bg-dark">
   <div class="container">
-    <span>Address: 5, Napoli Street</span>
+    <p>Address: 5, Napoli Street, Wellington 6011</p>
     <span>Email: pizzeriafortuna@mail.com</span>
     <span>Phone: 1800-834-5683</span>
     <p>&copy 2019 Pizzeria Fortuna</p>
