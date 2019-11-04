2d484516b80878dd83b6f22a05f2f695f7467255
Dharani <dharanich1985@gmail.com>
2019-11-04 17:04:59 +1300

    updated with order confirmation page and it's datasouce

--- php/orderConfirmation.php
+++ php/orderConfirmation.php
@@ -56,42 +56,78 @@ if(isset($_SESSION['uname']))
 <body style="background-color:black; color: white">
 <?php
 	//$orderId=$_GET['orderId'];
-	$orderId = 4;
+	//$orderId = 4;
 ?>
 <div class="container">
-		<div class="jumbotron" style="color: white" align="Center">
 		
-				<h1>Welcome To PizzaPlace</h1>
-		</div>
 	<center>
 	<div>
 		<?php
 				include('../php_components/orderConfirmDataSource.php');
+				$orderId=getOrderIdFromDB();
 				$custId=getCustomerIdByOrderId($orderId);
 				$custname=getNameByCustId($custId);
-				$memberId=getMemberIdByCustId($custId);
-				$email=getemailBymemberId($memberId);
+				$base=getOrderBase($orderId);
+				$status=getOrderStatus($orderId);
+				//$memberId=getMemberIdByCustId($custId);
+				//$email=getemailBymemberId($memberId);
 				$itemList=getItemListByOrderId($orderId);
 				
 		?>
 		<br/><br/>
 		<I>
 		<h1><u>Order Details</u></h1>	
-		<h1> Thank You :</h1>
-		<label> <?=$custname?></label>
-		<h1>Your Order number is  <?=$orderId?></h1>
+		<br/>
+		<h2> Thank You :       <?=$custname?></h2>
+		<br/>
+		<centre>
+		<table width="350px" border=1>
+		<tr>
+				<td align='center'><strong>Order number </strong></td>
+				<td align='center'><strong><?=$orderId?></strong></td>			
+		</tr>	
+		<tr>
+				<td align='center'><strong>Customer Id </strong></td>
+				<td align='center'><strong><?=$custId?></strong></td>			
+		</tr>	
+		<tr>
+				<td align='center'><strong>Order Base </strong></td>
+				<td align='center'><strong><?=$base?></strong></td>			
+		</tr>	
+		<tr>
+				<td align='center'><strong>Order Toppings </strong></td>
+				<td align='center'><strong>
+				<?php
+				//include('../php_components/orderConfirmDataSource.php');
+				printOrderList($itemList);
+				?>	
+				</strong></td>			
+		</tr>	
+		<tr>
+				<td align='center'><strong>Order Status </strong></td>
+				<td align='center'><strong><?=$status?></strong></td>			
+		</tr>	
+							
+		</table>
+		<br/><br/>
+		<h5> Christmas offers are coming soon, stay in touch</h5>
+	</I>
+	</centre>
+		<!--<h1>Your Order number is  <?=$orderId?></h1>
 		<h1>Your OrderList</h1>
 		<?php
 				//include('../php_components/orderConfirmDataSource.php');
 				printOrderList($itemList);
-		?>		
+		?>	
+		<h1>	
 		<h1>Check your email for order details</h1>
 		<h1><?=$email?><h1>
 		<h1>Your customer Id <?=$custId?></h1>
-		<!--<h1>Your memberId <?=$memberId?></h1>-->
-		</I>
+		<h1>Your memberId <?=$memberId?></h1>
+		</I>-->
 	</div>	
-	</center>	
+	</center>
+	
 </div>
 </body>
 </html>
--- php_components/orderConfirmDataSource.php
+++ php_components/orderConfirmDataSource.php
@@ -4,6 +4,89 @@ if(isset($_SESSION['uname']))
 {
   $uname=$_SESSION['uname'];
 }
+// method returns last orderId
+function getOrderIdFromDB()
+{
+	//database details
+	$dbhost = 'localhost';
+	$dbuser = 'myuser';
+	$dbpass = 'mypass';
+	$db_name =  'pizzadb';
+	//establish connection
+	$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $db_name);
+	if(! $conn ) {
+            die('Could not connect: ' . mysqli_error());
+      }
+    else
+	{
+		//find custID
+		$query= $conn->prepare("Select id from orders;");
+		try{
+			$query->execute();
+		$cresult = $query->get_result();
+		$ordersID=mysqli_num_rows($cresult);
+		}catch(Exception $e){ "The total number of rows in this table is $ordersID";
+		}
+	}	
+	return $ordersID;		
+}
+
+//get order base from order table
+function getOrderBase($orderId){
+
+	//database details
+	$dbhost = 'localhost';
+	$dbuser = 'myuser';
+	$dbpass = 'mypass';
+	$db_name =  'pizzadb';
+	//establish connection
+	$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $db_name);
+	if(! $conn ) {
+            die('Could not connect: ' . mysqli_error());
+      }
+    else
+	{
+		//echo 'Connected successfully';				
+			$query = $conn->prepare("SELECT * FROM orders WHERE id= ?;");
+			$query->bind_param("s", $orderId);
+			$query->execute();
+			$result = $query->get_result();				
+			$row = $result->fetch_assoc();
+			//print_r($row);
+			$base=$row['base'];					
+		}
+		return $base;	
+	}	
+		
+	
+
+// method returns status of the order from order table
+function getOrderStatus($orderId){
+	//database details
+	$dbhost = 'localhost';
+	$dbuser = 'myuser';
+	$dbpass = 'mypass';
+	$db_name =  'pizzadb';
+	//establish connection
+	$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $db_name);
+	if(! $conn ) {
+            die('Could not connect: ' . mysqli_error());
+      }
+    else
+	{
+		//echo 'Connected successfully';				
+			$query = $conn->prepare("SELECT * FROM orders WHERE id= ?;");
+			$query->bind_param("s", $orderId);
+			$query->execute();
+			$result = $query->get_result();				
+			$row = $result->fetch_assoc();
+			//print_r($row);
+			$status=$row['status'];					
+		}
+		return $status;	
+	}	
+	
+
 // method return cutomer's id from orders table using orderId
 function getCustomerIdByOrderId($orderId)
 {
