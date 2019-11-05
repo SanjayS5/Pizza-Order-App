d97fb537ae913bb189db186dc613922d23db9b0e
Sanjay Sudhakaran <sanjaysudhakaran@gmail.com>
2019-11-05 21:24:42 +1300

    Style changes on orderPage

--- js/orders.js
+++ js/orders.js
@@ -117,7 +117,7 @@ const addDetailsToOrder = () => {
      let memberId = 34404;
      if (document.querySelector('#memberId') == null) {
         alert("User not logged in or failure");
-        return;
+        memberId = 0;
     } else {
         memberId = document.querySelector('#memberId').innerText;
         memberId = parseInt(memberId, 10);
